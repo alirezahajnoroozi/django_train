139587ce9790bef44de01b434d6152749663a1ef
Alireza <alireza2012hnrzi@gmail.com>
2026-08-10 13:09:35 +0330

    makemigration and migrate, work with admin
    with migration, added db and made a superuser
    made Contact model in website app
    made Post model in blog app
    conected Post model to PostAdmin class in admin.py
    made some changes in admin panel by PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ["title", "status", "counted_views", "published_date"]
+    empty_value_display = "-empty-"
+
+admin.site.register(Post, PostAdmin)
