2514086c5317271e85bfb6c230653b859ff3551d
Alireza <alireza2012hnrzi@gmail.com>
2026-08-18 09:09:40 +0330

    changed admin panel view in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     list_display = ["title", "status", "counted_views", "published_date"]
     empty_value_display = "-empty-"
-
+    list_filter = ('status',)
+    ordering = ('-created_date',)
+    search_fields = ("title", "content")
 admin.site.register(Post, PostAdmin)
